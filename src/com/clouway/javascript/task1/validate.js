a2869842bbdcf4f32701e34c98a052c98a7118d8
Krasimir Dimitrov <kpackapgo@gmail.com>
2012-02-03 14:39:44 +0200

    validator without jquery

--- src/com/clouway/javascript/task1/validate.js
+++ src/com/clouway/javascript/task1/validate.js
@@ -1,86 +1,62 @@
-$(document).ready(function(){
+function validateFirstName() {
+    if (document.getElementById('firstName').value.length < 5) {
+        alert("First name must be at least 5 characters");
+        return false;
+    } else return true;
+}
 
-var jVal = {
+function validateLastName() {
+    if (document.getElementById('lastName').value.length < 5) {
+        alert("Last name must be at least 5 characters");
+        return false;
+    } else return true;
+}
 
-    'sendIt' : function (){
-        if(!jVal.errors){
-            alert("SENT");
-            $('#regForm').submit();
-        }
-    },
+function validateEGN() {
+    if ((document.getElementById('egn').value.length < 10) || (isNaN(document.getElementById('egn').value))) {
+        alert("egn must me only from numbers and must be 10 chars");
+        return false;
+    } else return true;
+}
 
-    'firstName' : function() {
+function validateAge() {
+    if ((document.getElementById('age').value < 18) || (document.getElementById('age').value > 118) || (isNaN(document.getElementById('age').value))) {
+        alert("Age must be between 18 and 118 and must be numbers only");
+        return false;
+    }
+    return true;
+}
 
-        $('#mainDiv').append('<div id="firstNameInfo" class="info"></div>');
+function validatePassword() {
+    var pattern = /^[a-zA-Z0-9]{6,18}$/;
+    if (!pattern.test(document.getElementById('password').value)) {
+        alert("Only numbers and a-z A-Z symbols allowed!");
+        return false;
+    }
+    return true;
+}
 
-        var firstNameInfo = $('#firstNameInfo');
-        var ele=$('#firstName');
-        var pos = ele.offset();
+function matchPassword() {
+    var pass = new String(document.getElementById('password').value);
+    var passConfirm = new String(document.getElementById('confirmPassword').value);
+    if (pass.localeCompare(passConfirm) != 0) {
+        alert("Passwords doesn't match");
+        return false;
+    }
+    return true;
+}
 
-       firstNameInfo.css({
-                'position' : 'absolute',
-                'color' : 'red',
-                'background-color' : '#ddd',
-				'left': '300px',
-				'top': pos.top-ele.height()
-			});
-
-			if(ele.val().length < 5) {
-				jVal.errors = true;
-					firstNameInfo.removeClass('correct').addClass('error').html('&larr; at least 5 characters').show();
-					ele.removeClass('normal').addClass('wrong');
-			} else {
-					firstNameInfo.removeClass('error').addClass('correct').html('&radic;').show();
-					ele.removeClass('wrong').addClass('normal');
-			}
-		},
-
-
-
-
-		'lastName' : function() {
-
-        $('#mainDiv').append('<div id="lastNameInfo" class="info"></div>');
-
-        var lastNameInfo = $('#lastNameInfo');
-        var ele=$('#lastName');
-        var pos = ele.offset();
-
-        lastNameInfo.css({
-                'position' : 'absolute',
-                'color' : 'red',
-                'background-color' : '#ddd',
-				'left': '300px',
-				'top': pos.top-ele.height()
-			});
-
-			if(ele.val().length < 5) {
-				jVal.errors = true;
-					lastNameInfo.removeClass('correct').addClass('error').html('&larr; at least 5 characters').show();
-					ele.removeClass('normal').addClass('wrong');
-			} else {
-					lastNameInfo.removeClass('error').addClass('correct').html('&radic;').show();
-					ele.removeClass('wrong').addClass('normal');
-			}
-		}
-
-
-
-};
-
-
-
-  $('#send').click(function (){
-        var obj = $.browser.webkit ? $('body') : $('html');
-		obj.animate({ scrollTop: $('#regForm').offset().top }, 750, function (){
-			jVal.errors = false;
-			jVal.firstName();
-			jVal.lastName();
-			jVal.sendIt();
-		});
-		return false;
-	});
-
-});
+function sendIt() {
+    if (validateFirstName() && validateLastName() && validateEGN() && validateAge() && validatePassword() && matchPassword()) {
+        document.getElementById('regForm').submit();
+    } else {
+        validateFirstName();
+        validateLastName();
+        validateEGN();
+        validateAge();
+        validatePassword();
+        matchPassword();
+    }
+}
 
 
